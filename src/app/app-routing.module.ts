bad094c3b9a196d965dc952cf1b61ac92231ec11
Sridhar Kumaresan <sridharkumaresan@gmail.com>
2023-01-02 23:20:13 -0500

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { LazyModuleOneComponent } from './lazy-module-one/lazy-module-one.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {
+        path: 'lazy-one',
+        loadChildren: () =>
+          import('./lazy-module-one/lazy-module-one.module').then(
+            (m) => m.LazyModuleOneModule
+          ),
+      },
+    ],
+  },
+  {
+    path: '',
+    redirectTo: 'micro-app-two',
+    pathMatch: 'full',
+  },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
